5a5690e381f04340beadf3843ce2f89f9f87ab76
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-08-27 17:03:22 -0300

    Catch request error

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,11 +2,11 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ButtonInverse from "../../../components/ButtonInverse";
 import * as productService from '../../../services/ProductService'
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/Product";
-import axios from "axios";
+import './styles.css'
 
 
 export default function ProductDetails() {
@@ -14,6 +14,7 @@ export default function ProductDetails() {
   const params = useParams();
 
   const [product, setProduct] = useState<ProductDTO>();
+  const navigate = useNavigate();
 
   useEffect(() => {
     productService.findById(Number(params.productId))
@@ -21,14 +22,16 @@ export default function ProductDetails() {
         console.log(response.data)
         setProduct(response.data);
       })
-
+      .catch(() => {
+        navigate('/');
+      })
 
   }, [params])
 
   return (
     <>
       <main>
-        <section id="product-details-section" className="dsc-container">
+        <section className="dsc-container" id="product-details-section">
           {
             product &&
             <ProductDetailsCard product={product} />
