f089016524b95d7c07baf61c3f1544592b14cc03
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-10-18 21:22:01 -0300

    Redirecting outside React component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import * as cartService from './services/CartService'
 import Login from "./routes/ClientHome/Login";
 import Admin from "./routes/Admin";
 import AdminHome from "./routes/Admin/AdminHome";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { history } from "./utils/history";
 
 
 function App() {
@@ -19,7 +21,7 @@ function App() {
 
   return (
     <ContextCartCount.Provider value={{ contextCartCount, setContextCartCount }}>
-      <BrowserRouter>
+      <HistoryRouter history={history}>
         <Routes>
           <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
@@ -33,7 +35,7 @@ function App() {
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </BrowserRouter>
+      </HistoryRouter>
     </ContextCartCount.Provider>
   );
 }
